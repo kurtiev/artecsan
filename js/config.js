eabd653fdfd3403943edba85c6efaedf12ddce5a
Fikret Kurtiev <fikretkurtiev@gmail.com>
2017-01-03 17:10:00 +0200

    Init

---
+++ js/config.js
@@ -0,0 +1,320 @@
+/**
+ * INSPINIA - Responsive Admin Theme
+ *
+ * Inspinia theme use AngularUI Router to manage routing and views
+ * Each view are defined as state.
+ * Initial there are written state for all view in theme.
+ *
+ */
+function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, IdleProvider, KeepaliveProvider) {
+
+    // Configure Idle settings
+    IdleProvider.idle(5); // in seconds
+    IdleProvider.timeout(120); // in seconds
+
+    $urlRouterProvider.otherwise("/logins");
+
+    $ocLazyLoadProvider.config({
+        // Set to true if you want to see what and when is dynamically loaded
+        debug: false
+    });
+
+    $stateProvider
+
+        .state('logins', {
+            url: "/logins",
+            templateUrl: "views/login.html",
+            data: {pageTitle: 'Login', specialClass: 'login-page'},
+        })
+        .state('admin', {
+            abstract: true,
+            url: "/admin",
+            templateUrl: "views/common/content.html",
+        })
+        .state('home', {
+            abstract: true,
+            url: "/home",
+            templateUrl: "views/common/content_home.html",
+        })
+        .state('home.home', {
+            url: "/home",
+            templateUrl: "views/home.html",
+            data: {pageTitle: 'Home'},
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+                            files: ['js/plugins/footable/footable.all.min.js', 'css/plugins/footable/footable.core.css']
+                        },
+                        {
+                            name: 'ui.footable',
+                            files: ['js/plugins/footable/angular-footable.js']
+                        }
+                    ]);
+                }
+            }
+        })
+        .state('admin.homeMenu', {
+            url: "/home_menu",
+            templateUrl: "views/homeMenu.html",
+            data: {pageTitle: 'Home'},
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+                            files: ['js/plugins/footable/footable.all.min.js', 'css/plugins/footable/footable.core.css']
+                        },
+                        {
+                            name: 'ui.footable',
+                            files: ['js/plugins/footable/angular-footable.js']
+                        }
+                    ]);
+                }
+            }
+        })
+        .state('admin.administrator', {
+            url: "/administrator",
+            templateUrl: "views/administrator.html",
+            data: {pageTitle: 'Administrator'}
+        })
+        .state('admin.managementCategories', {
+            url: "/management_categories",
+            templateUrl: "views/managementCategories.html",
+            data: {pageTitle: 'Management Categories'}
+        })
+        .state('admin.inventoryCategories', {
+            url: "/inventory_categories",
+            templateUrl: "views/inventoryCategories.html",
+            data: {pageTitle: 'Inventory Categories'}
+        })
+        .state('admin.pos', {
+            url: "/pos",
+            templateUrl: "views/posSelectionScreen.html",
+            data: {pageTitle: 'POS'}
+        })
+        .state('admin.comingSoon', {
+            url: "/coming_soon",
+            templateUrl: "views/comingSoon.html",
+            data: {pageTitle: 'Coming Soon'}
+        })
+        .state('admin.posSync', {
+            url: "/pos_sync",
+            templateUrl: "views/posSync.html",
+            data: {pageTitle: 'POS Sync'}
+        })
+        .state('admin.dashboardSubCategories', {
+            url: "/dashboard_sub_categories",
+            templateUrl: "views/dashboardSubCategories.html",
+            data: {pageTitle: 'Dashboard Sub Categories'}
+        })
+        .state('admin.addPosHere', {
+            url: "/add_pos_here",
+            templateUrl: "views/addPosHere.html",
+            data: {pageTitle: 'Add Pos Here'}
+        })
+        .state('admin.greatJob', {
+            url: "/great_job",
+            templateUrl: "views/greatJob.html",
+            data: {pageTitle: 'Great Job'}
+        })
+        .state('admin.posDetails', {
+            url: "/pos_details",
+            templateUrl: "views/posDetails.html",
+            data: {pageTitle: 'POS'},
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+                            files: ['js/plugins/wow/wow.min.js']
+                        },
+                        {
+                            name: 'cgNotify',
+                            files: ['css/plugins/angular-notify/angular-notify.min.css', 'js/plugins/angular-notify/angular-notify.min.js']
+                        }
+                    ]);
+                }
+            }
+        })
+
+        .state('profile', {
+            abstract: true,
+            url: "/profile",
+            templateUrl: "views/common/content.html",
+        })
+        .state('profile.setupProfile', {
+            url: "/setup_profile",
+            templateUrl: "views/ownerProfileSetup.html",
+            data: {pageTitle: 'Profile'}
+        })
+        .state('profile.restaurantProfile', {
+            url: "/restaurant_profile",
+            templateUrl: "views/restaurantProfile.html",
+            data: {pageTitle: 'Restaurant Profile'}
+        })
+        .state('profile.paymentProfile', {
+            url: "/payment_profile",
+            templateUrl: "views/paymentProfile.html",
+            data: {pageTitle: 'Payment Profile'}
+        })
+        .state('profile.userSetup', {
+            url: "/user_setup",
+            templateUrl: "views/userSetup.html",
+            data: {pageTitle: 'User Setup'}
+        })
+        .state('food', {
+            abstract: true,
+            url: "/food",
+            templateUrl: "views/common/content.html",
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+                            files: ['js/plugins/footable/footable.all.min.js', 'css/plugins/footable/footable.core.css']
+                        },
+                        {
+                            name: 'ui.footable',
+                            files: ['js/plugins/footable/angular-footable.js']
+                        }
+                    ]);
+                }
+            }
+        })
+        .state('food.vendorSetup', {
+            url: "/vendor_setup",
+            templateUrl: "views/vendorSetup.html",
+            data: {pageTitle: 'Vendor Setup'}
+        })
+        .state('food.agreement', {
+            url: "/agreement",
+            templateUrl: "views/agreement.html",
+            data: {pageTitle: 'Agreement'}
+        })
+        .state('food.setupConfirmation', {
+            url: "/setup_confirmation",
+            templateUrl: "views/setupConfirmation.html",
+            data: {pageTitle: 'Setup Confirmation'}
+        })
+        .state('food.inventorySetup', {
+            url: "/inventory_setup",
+            templateUrl: "views/inventorySetup.html",
+            data: {pageTitle: 'Inventory Setup'}
+        })
+        .state('food.foodInventory', {
+            url: "/food_inventory",
+            templateUrl: "views/foodInventory.html",
+            data: {pageTitle: 'Food Inventory'}
+        })
+        .state('food.recipeSetup', {
+            url: "/recipe_setup",
+            templateUrl: "views/recipeSetup.html",
+            data: {pageTitle: 'Recipe Setup'}
+        })
+        .state('food.menuSetup', {
+            url: "/menu_setup",
+            templateUrl: "views/menuSetup.html",
+            data: {pageTitle: 'Menu Setup'}
+        })
+        .state('food.deliverySetup', {
+            url: "/delivery_setup",
+            templateUrl: "views/deliverySetup.html",
+            data: {pageTitle: 'Delivery Setup'}
+        })
+        .state('food.foodSubCategories', {
+            url: "/food_sub_categories",
+            templateUrl: "views/foodSubCategories.html",
+            data: {pageTitle: 'Food Sub Categories'}
+        })
+        .state('food.newFoodOrder', {
+            url: "/new_food_order",
+            templateUrl: "views/newFoodOrder.html",
+            data: {pageTitle: 'New Food Order'}
+        })
+        .state('food.orderSummary', {
+            url: "/order_summary",
+            templateUrl: "views/orderSummary.html",
+            data: {pageTitle: 'Order Summary'}
+        })
+        .state('food.performanceScore', {
+            url: "/performance_score",
+            templateUrl: "views/performanceScore.html",
+            data: {pageTitle: 'Performance Score'}
+        })
+        .state('food.foodDetail', {
+            url: "/food_detail",
+            templateUrl: "views/foodDetail.html",
+            data: {pageTitle: 'Food Detail'}
+        })
+        .state('food.inventoryUsage', {
+            url: "/inventory_usage",
+            templateUrl: "views/inventoryUsage.html",
+            data: {pageTitle: 'Inventory Usage'},
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+                            files: ['js/plugins/moment/moment.min.js']
+                        },
+                        {
+                            name: 'datePicker',
+                            files: ['css/plugins/datapicker/angular-datapicker.css', 'js/plugins/datapicker/angular-datepicker.js']
+                        }
+
+                    ]);
+                }
+            }
+        })
+        .state('food.inventoryPurchases', {
+            url: "/inventory_purchases",
+            templateUrl: "views/inventoryPurchases.html",
+            data: {pageTitle: 'Inventory Purchases'},
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+                            files: ['js/plugins/moment/moment.min.js']
+                        },
+                        {
+                            name: 'datePicker',
+                            files: ['css/plugins/datapicker/angular-datapicker.css', 'js/plugins/datapicker/angular-datepicker.js']
+                        }
+
+                    ]);
+                }
+            }
+        })
+        .state('food.costOfSales', {
+            url: "/cost_of_sales",
+            templateUrl: "views/costOfSales.html",
+            data: {pageTitle: 'Cost Of Sales'},
+            resolve: {
+                loadPlugin: function ($ocLazyLoad) {
+                    return $ocLazyLoad.load([
+                        {
+                            files: ['js/plugins/moment/moment.min.js']
+                        },
+                        {
+                            name: 'datePicker',
+                            files: ['css/plugins/datapicker/angular-datapicker.css', 'js/plugins/datapicker/angular-datepicker.js']
+                        }
+
+                    ]);
+                }
+            }
+        })
+        .state('user', {
+            abstract: true,
+            url: "/user",
+            templateUrl: "views/common/content-new-user.html",
+        })
+        .state('user.createAccount', {
+            url: "/create_Account",
+            templateUrl: "views/ownerProfileSetup.html",
+            data: {pageTitle: 'Profile'}
+        })
+}
+angular
+    .module('inspinia')
+    .config(config)
+    .run(function ($rootScope, $state) {
+        $rootScope.$state = $state;
+    });
---
+++ js/controllers.js
@@ -0,0 +1,550 @@
+/**
+ * MainCtrl - controller
+ * Contains several global data used in different view
+ *
+ */
+function MainCtrl($http, $uibModal, $scope) {
+    this.posIndex = 2;
+    this.isRegistration = false;
+    this.isFoodSetup = false;
+    this.isTrue = true;
+    this.type = {
+        value: 1,
+        name: 'MEAT & POULTRY'
+    };
+
+    this.types = [{
+        value: 1,
+        name: 'MEAT & POULTRY'
+    }, {
+        value: 2,
+        name: 'SEAFOOD'
+    },
+        {
+            value: 3,
+            name: 'PRODUCE'
+        },
+        {
+            value: 4,
+            name: 'VEGETABLES'
+        },
+        {
+            value: 5,
+            name: 'BREAD'
+        },
+        {
+            value: 6,
+            name: 'FRUIT'
+        }
+
+    ]
+
+    this.usersList = [{
+        firstName: 'firstName',
+        lastName: 'lastName',
+        email: 'test123@ttts.rt',
+        userType: 'Admin',
+        selected: false
+    },
+        {
+            firstName: 'firstName2',
+            lastName: 'lastName',
+            email: 'test123@ttts.rt',
+            userType: 'Admin',
+            selected: false
+        },
+        {
+            firstName: 'firstName3',
+            lastName: 'lastName',
+            email: 'test123@ttts.rt',
+            userType: 'Admin',
+            selected: false
+        }];
+    this.vendorsList = [{
+        id: 121,
+        name: 'test',
+        city: 'City',
+        date: '12/12/12',
+        zip: '123',
+        selected: false
+    },
+        {
+            id: 122,
+            name: 'test6',
+            city: 'City6',
+            date: '12/12/16',
+            zip: '1236',
+            selected: false
+        },
+        {
+            id: 1241,
+            name: 'test3',
+            city: 'City3',
+            date: '12/15/12',
+            zip: '1523',
+            selected: false
+        },
+        {
+            id: 1621,
+            name: 'test8',
+            city: 'City8',
+            date: '12/12/13',
+            zip: '1239',
+            selected: false
+        },
+        {
+            id: 1821,
+            name: 'test3',
+            city: 'City3',
+            date: '12/12/18',
+            zip: '1235',
+            selected: false
+        },
+        {
+            id: 18621,
+            name: 'test3',
+            city: 'City3',
+            date: '12/12/18',
+            zip: '1235',
+            selected: false
+        }];
+
+    this.selectedVendors = [{
+        id: 121,
+        name: 'test',
+        city: 'City',
+        date: '12/12/12',
+        zip: '123',
+        selected: false
+    },
+        {
+            id: 122,
+            name: 'test',
+            city: 'City',
+            date: '12/12/12',
+            zip: '123',
+            selected: false
+        }];
+
+    this.selectedInventoryList = [];
+    this.inventoryList = [{
+        id: 18321,
+        name: 'test',
+        desc: 'description',
+        selected: false
+    },
+        {
+            id: 14821,
+            name: 'test',
+            desc: 'description',
+            selected: false
+        },
+        {
+            id: 15821,
+            name: 'test',
+            desc: 'description',
+            selected: false
+        },
+        {
+            id: 17821,
+            name: 'test',
+            desc: 'description',
+            selected: false
+        },
+        {
+            id: 18821,
+            name: 'test',
+            desc: 'description',
+            selected: false
+        }]
+
+    this.selectInventory = function (ind) {
+        this.inventoryList[ind].selected = !this.inventoryList[ind].selected;
+    }
+
+    this.selectVendor = function (ind) {
+        this.vendorsList[ind].selected = !this.vendorsList[ind].selected;
+    }
+
+    this.recipeList = [
+        {
+            name: 'recipe1',
+            type: 'type1',
+            cnt: 123
+        },
+        {
+            name: 'recipe19',
+            type: 'type19',
+            cnt: 1923
+        },
+        {
+            name: 'recipe1',
+            type: 'type1',
+            cnt: 123
+        },
+        {
+            name: 'recipe18',
+            type: 'type18',
+            cnt: 1283
+        },
+        {
+            name: 'recipe16',
+            type: 'type16',
+            cnt: 1623
+        },
+        {
+            name: 'recipe41',
+            type: 'type41',
+            cnt: 1523
+        }];
+    this.menuList = [
+        {
+            id: 1,
+            name: 'menu item',
+            price: 23,
+            cost: 123
+        },
+        {
+            id: 41,
+            name: 'menu item',
+            price: 243,
+            cost: 123
+        }, {
+            id: 15,
+            name: 'menu item',
+            price: 23,
+            cost: 123
+        }, {
+            id: 16,
+            name: 'menu item',
+            price: 23,
+            cost: 123
+        }, {
+            id: 71,
+            name: 'menu item',
+            price: 23,
+            cost: 123
+        }, {
+            id: 18,
+            name: 'menu item',
+            price: 23,
+            cost: 123
+        }, {
+            id: 19,
+            name: 'menu item',
+            price: 23,
+            cost: 123
+        }, {
+            id: 231,
+            name: 'menu item',
+            price: 23,
+            cost: 123
+        }];
+
+    this.deliveryList = [{
+        c: 'Category',
+        d: '	Mon,Tue,Wed'
+    },
+        {
+            c: 'Category',
+            d: '	Mon,Tue,Wed'
+        },
+        {
+            c: 'Category',
+            d: '	Mon,Tue,Wed'
+        },
+        {
+            c: 'Category',
+            d: '	Mon,Tue,Wed'
+        }]
+
+    this.selectBlock = function (val) {
+        this.posIndex = val;
+    };
+
+    this.selectRestaurant = function () {
+        this.isRestaurantSelected = true;
+    };
+
+    this.logout = function () {
+        this.posIndex = 2;
+        this.isRegistration = false;
+        this.isFoodSetup = false;
+    };
+
+    this.reset = function () {
+        this.posIndex = 2;
+        this.isRestaurantSelected = false;
+        this.changeFirstDashboaed = false;
+    }
+
+
+    this.open2 = function (self) {
+        if (!self.usersList)
+            self.usersList = [];
+
+        var modalInstance = $uibModal.open({
+            templateUrl: 'views/modal/add_new_user_item.html',
+            controller: ModalInstanceCtrl,
+            windowClass: "animated fadeIn",
+            controllerAs: 'vm',
+            resolve: {
+                user: function () {
+                    return null;
+                }
+            }
+        });
+
+        modalInstance.result.then(function (result) {
+            self.usersList.push(result);
+        }, function (reason) {
+
+        });
+    };
+
+
+    this.open3 = function (self) {
+        if (!self.usersList)
+            self.usersList = [];
+
+        var modalInstance = $uibModal.open({
+            templateUrl: 'views/modal/invite_new_user_item.html',
+            controller: ModalInstanceCtrl,
+            windowClass: "animated fadeIn",
+            controllerAs: 'vm',
+            resolve: {
+                user: function () {
+                    return null;
+                }
+            }
+        });
+
+        modalInstance.result.then(function (result) {
+            self.usersList.push(result);
+        }, function (reason) {
+
+        });
+    };
+
+    this.open4 = function (self) {
+        var modalInstance = $uibModal.open({
+            templateUrl: 'views/modal/add_new_recipe_item.html',
+            controller: ModalInstanceRecipeCtrl,
+            windowClass: "animated fadeIn",
+            controllerAs: 'vm',
+            resolve: {
+                recipe: function () {
+                    return null;
+                }
+            }
+        });
+
+        modalInstance.result.then(function (result) {
+
+        }, function (reason) {
+
+        });
+    };
+
+    this.open6 = function (self) {
+        var modalInstance = $uibModal.open({
+            templateUrl: 'views/modal/add_new_menu_item.html',
+            controller: ModalInstanceMenuCtrl,
+            windowClass: "animated fadeIn",
+            controllerAs: 'vm',
+            resolve: {
+                menu: function () {
+                    return null;
+                }
+            }
+        });
+
+        modalInstance.result.then(function (result) {
+
+        }, function (reason) {
+
+        });
+    };
+    this.open7 = function (self) {
+        var modalInstance = $uibModal.open({
+            templateUrl: 'views/modal/add_new_menu_item.html',
+            controller: ModalInstanceMenuCtrl,
+            windowClass: "animated fadeIn",
+            controllerAs: 'vm',
+            resolve: {
+                menu: function () {
+                    return self.menuList[0];
+                }
+            }
+        });
+
+        modalInstance.result.then(function (result) {
+
+        }, function (reason) {
+
+        });
+    };
+
+
+    this.open8 = function (self) {
+        var modalInstance = $uibModal.open({
+            templateUrl: 'views/modal/add_new_delivery_item.html',
+            controller: ModalInstanceDeliveryCtrl,
+            windowClass: "animated fadeIn",
+            controllerAs: 'vm'
+        });
+
+        modalInstance.result.then(function (result) {
+
+        }, function (reason) {
+
+        });
+    };
+
+    this.editUser = function (self, user, index) {
+        self.usersList.splice(index, 1);
+        var modalInstance = $uibModal.open({
+            templateUrl: 'views/modal/add_new_user_item.html',
+            controller: ModalInstanceCtrl,
+            windowClass: "animated fadeIn",
+            controllerAs: 'vm',
+            resolve: {
+                user: function () {
+                    return user;
+                }
+            }
+        });
+
+        modalInstance.result.then(function (result) {
+            self.usersList.push(result);
+        }, function (reason) {
+
+        });
+    };
+
+    this.deleteUser = function (self, index) {
+        self.usersList.splice(index, 1);
+    }
+
+    this.where = function () {
+        alert('Under construction');
+    }
+
+    this.willBe = function () {
+        alert('It will work if number of vendors > 1');
+    }
+};
+
+function notifyCtrl($scope, notify, $state) {
+    $scope.inspiniaTemplate = 'views/common/notify.html';
+    $scope.inspiniaDemo1 = function () {
+        notify({
+            message: 'Thank you! We will contact you soon',
+            classes: 'alert-info',
+            templateUrl: $scope.inspiniaTemplate
+        });
+        $state.go('home.home');
+    }
+
+}
+
+function ModalInstanceCtrl($scope, $uibModalInstance, user) {
+
+    this.isEdit = false;
+    this.user = user;
+    if (user)
+        this.isEdit = true;
+    $scope.ok = function (user) {
+        $uibModalInstance.close(user);
+    };
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+};
+function ModalInstanceDeliveryCtrl($scope, $uibModalInstance) {
+
+
+    $scope.ok = function (user) {
+        $uibModalInstance.close();
+    };
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+};
+
+function ModalInstanceRecipeCtrl($scope, $uibModalInstance, recipe) {
+
+    this.isEdit = false;
+    this.recipe = recipe;
+    if (recipe)
+        this.isEdit = true;
+    $scope.ok = function (recipe) {
+        $uibModalInstance.close(recipe);
+    };
+
+    this.ingredients = [{
+        amount: 122,
+        cost: 300
+    },
+        {
+            amount: 1242,
+            cost: 3060
+        }]
+
+    this.addIngredient = function () {
+        this.ingredients.push({
+            amount: 1622,
+            cost: 3070
+        });
+    }
+
+    this.removeIngredient = function (ind) {
+        this.ingredients.splice(ind, 1);
+    }
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+};
+
+
+function ModalInstanceMenuCtrl($scope, $uibModalInstance, menu) {
+
+    this.isEdit = false;
+    this.menu = menu;
+    if (menu)
+        this.isEdit = true;
+
+    $scope.recipeInventories = [];
+    $scope.ok = function (menu) {
+        $uibModalInstance.close(menu);
+    };
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+
+    this.addRecipeInventory = function () {
+        $scope.recipeInventories.push({
+            name: 'recipe'
+        });
+    }
+    this.remove = function (ind) {
+        $scope.recipeInventories.splice(ind, 1);
+    }
+};
+
+
+/**
+ *
+ * Pass all functions into module
+ */
+angular
+    .module('inspinia')
+    .controller('MainCtrl', MainCtrl)
+    .controller('notifyCtrl', notifyCtrl)
+    .controller('ModalInstanceRecipeCtrl', ModalInstanceRecipeCtrl)
+    .controller('ModalInstanceMenuCtrl', ModalInstanceMenuCtrl)
+    .controller('ModalInstanceDeliveryCtrl', ModalInstanceDeliveryCtrl);
+
