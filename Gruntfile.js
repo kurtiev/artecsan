46abc91fe5e40133ca798e5d66793941e36f16e2
Fikret Kurtiev <fikretkurtiev@gmail.com>
2017-01-31 19:23:58 +0200

    grunt replace

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,49 +2,14 @@
 
 module.exports = function (grunt) {
 
-    var server = function () {
-        var express = require('express');
-        var app = express();
-        var path = require('path');
-        app.get('/', function(req, res) {
-            app.use("/dist",express.static(__dirname + "/dist"));
-            app.use("/build",express.static(__dirname + "/build"));
-            app.use("/app",express.static(__dirname + "/app"));
-            app.use("/views",express.static(__dirname + "/views"));
-            res.sendFile(path.join(__dirname + '/index.html'));
-        });
-
-        app.listen(3001);
-    };
-
-    //Main App
-    var appJS = ['js/components/**/*.js'];
-
     grunt.initConfig({
-        uglify: {
-            options: {
-                mangle: false
-            },
-            app_files: {
-                files: {
-                    'build/js/components.min.js': appJS
-                }
-            }
-        },
-        cssmin: {
-            target: {
-                files: {
-                    'build/css/styles.min.css': ['build/css/styles.min.css']
-                }
-            }
-        },
         replace: {
             build_replace: {
                 options: {
                     variables: {
                         // Generate a truly random number by concatenating the current date with a random number
                         // The variable name corresponds with the same in our HTML file
-                        'hash': '<%= ((new Date()).valueOf().toString()) + (Math.floor((Math.random()*1000000)+1).toString()) %>'
+                        'hash': '<%= ((new Date()).getTime()) %>'
                     }
                 },
                 // Source and destination files
@@ -55,35 +20,12 @@ module.exports = function (grunt) {
                     }
                 ]
             }
-        },
-        watch: {
-            devState: {
-                files: appJS,
-                tasks: 'default',
-                options: {}
-            }
-        },
-        express: {
-            options: {
-                background: true,
-                port: 3000
-            },
-            dev: {
-                options: {
-                    script: 'server.js'
-                }
-            }
         }
     });
 
-
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-replace');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-express-server');
 
 
-    grunt.registerTask('default', ['uglify', 'replace']);
-    grunt.registerTask('dev', ['express:dev', 'watch']);
+    grunt.registerTask('default', ['replace']);
 
 };
