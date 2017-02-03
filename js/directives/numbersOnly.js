fd8ea35efa4ee0f3b5f05297ad00e7966fc24b98
Eugen Protsenko <protsenkoe@digicode.net>
2017-02-03 10:12:27 +0200

    ARS-12: FE: creditcard payment screen the CVV code doesn't accept a 0 at the first number

---
+++ js/directives/numbersOnly.js
@@ -0,0 +1,26 @@
+function numbersOnly () {
+    return {
+        require: '?ngModel',
+        link: function (scope, element, attrs, ngModelCtrl) {
+
+            function fromUser(text) {
+                if (text) {
+                    var transformedInput = text.replace(/[^0-9]/g, '');
+
+                    if (transformedInput !== text) {
+                        ngModelCtrl.$setViewValue(transformedInput);
+                        ngModelCtrl.$render();
+                    }
+                    return transformedInput;
+                }
+                return undefined;
+            }
+            ngModelCtrl.$parsers.push(fromUser);
+
+        }
+    };
+}
+
+angular
+    .module('inspinia')
+    .directive('numbersOnly', numbersOnly);
