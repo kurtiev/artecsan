3185e26c7dc1e68f536bec91c5ac975a73bd2e13
Fikret Kurtiev <fikretkurtiev@gmail.com>
2017-02-22 15:49:11 +0200

    Delete unneeded wire frames

--- js/config.js
+++ js/config.js
@@ -23,50 +23,12 @@ function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, IdlePro
         debug: false
     });
 
-     $stateProvider
-        // .state('login', {
-        //     url: "/login",
-        //     template: "<login-component></login-component>",
-        //     data: {pageTitle: 'Login', specialClass: 'login-page'},
-        //     resolve: {
-        //         loadPlugin: function ($ocLazyLoad) {
-        //             return $ocLazyLoad.load([
-        //                 {
-        //                     name: 'vcRecaptcha',
-        //                     files: ['js/plugins/angular-recaptcha.min.js']
-        //                 }
-        //             ]);
-        //         }
-        //     }
-        // })
+    $stateProvider
         .state('admin', {
             abstract: true,
             url: "/admin",
-            templateUrl: "views/common/content.html",
+            templateUrl: "views/common/content.html"
         })
-        // .state('home', {
-        //     abstract: true,
-        //     url: "/home",
-        //     templateUrl: "views/common/content_home.html",
-        // })
-        // .state('home.home', {
-        //     url: "/home",
-        //     templateUrl: "views/home.html",
-        //     data: {pageTitle: 'Home'},
-        //     resolve: {
-        //         loadPlugin: function ($ocLazyLoad) {
-        //             return $ocLazyLoad.load([
-        //                 {
-        //                     files: ['js/plugins/footable/footable.all.min.js', 'css/plugins/footable/footable.core.css']
-        //                 },
-        //                 {
-        //                     name: 'ui.footable',
-        //                     files: ['js/plugins/footable/angular-footable.js']
-        //                 }
-        //             ]);
-        //         }
-        //     }
-        // })
         .state('admin.homeMenu', {
             url: "/home_menu",
             templateUrl: "views/homeMenu.html",
@@ -95,11 +57,6 @@ function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, IdlePro
                 }
             }
         })
-        // .state('admin.administrator', {
-        //     url: "/administrator",
-        //     templateUrl: "views/administrator.html",
-        //     data: {pageTitle: 'Administrator'}
-        // })
         .state('admin.managementCategories', {
             url: "/management_categories",
             templateUrl: "views/managementCategories.html",
@@ -110,75 +67,15 @@ function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, IdlePro
             templateUrl: "views/inventoryCategories.html",
             data: {pageTitle: 'Inventory Categories'}
         })
-        .state('admin.pos', {
-            url: "/pos",
-            templateUrl: "views/posSelectionScreen.html",
-            data: {pageTitle: 'POS'}
-        })
         .state('admin.comingSoon', {
             url: "/coming_soon",
             templateUrl: "views/comingSoon.html",
             data: {pageTitle: 'Coming Soon'}
         })
-        // .state('admin.posSync', {
-        //     url: "/pos_sync",
-        //     templateUrl: "views/posSync.html",
-        //     data: {pageTitle: 'POS Sync'}
-        // })
         .state('admin.dashboardSubCategories', {
             url: "/dashboard_sub_categories",
             templateUrl: "views/dashboardSubCategories.html",
             data: {pageTitle: 'Dashboard Sub Categories'}
-        })
-        // .state('admin.addPosHere', {
-        //     url: "/add_pos_here",
-        //     templateUrl: "views/addPosHere.html",
-        //     data: {pageTitle: 'Add Pos Here'}
-        // })
-        .state('admin.greatJob', {
-            url: "/great_job",
-            templateUrl: "views/greatJob.html",
-            data: {pageTitle: 'Great Job'}
-        })
-        .state('admin.posDetails', {
-            url: "/pos_details",
-            templateUrl: "views/posDetails.html",
-            data: {pageTitle: 'POS'},
-            resolve: {
-                loadPlugin: function ($ocLazyLoad) {
-                    return $ocLazyLoad.load([
-                        {
-                            files: ['js/plugins/wow/wow.min.js']
-                        }
-                    ]);
-                }
-            }
-        })
-
-        .state('profile', {
-            abstract: true,
-            url: "/profile",
-            templateUrl: "views/common/content.html",
-        })
-        .state('profile.setupProfile', {
-            url: "/setup_profile",
-            templateUrl: "views/ownerProfileSetup.html",
-            data: {pageTitle: 'Profile'}
-        })
-        .state('profile.restaurantProfile', {
-            url: "/restaurant_profile",
-            templateUrl: "views/restaurantProfile.html",
-            data: {pageTitle: 'Restaurant Profile'}
-        })
-        .state('profile.paymentProfile', {
-            url: "/payment_profile",
-            templateUrl: "views/paymentProfile.html",
-            data: {pageTitle: 'Payment Profile'}
-        })
-        .state('profile.userSetup', {
-            url: "/user_setup",
-            templateUrl: "views/userSetup.html",
-            data: {pageTitle: 'User Setup'}
         })
         .state('food', {
             abstract: true,
@@ -198,45 +95,10 @@ function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, IdlePro
                 }
             }
         })
-        .state('food.vendorSetup', {
-            url: "/vendor_setup",
-            templateUrl: "views/vendorSetup.html",
-            data: {pageTitle: 'Vendor Setup'}
-        })
-        .state('food.agreement', {
-            url: "/agreement",
-            templateUrl: "views/agreement.html",
-            data: {pageTitle: 'Agreement'}
-        })
         .state('food.setupConfirmation', {
             url: "/setup_confirmation",
             templateUrl: "views/setupConfirmation.html",
             data: {pageTitle: 'Setup Confirmation'}
-        })
-        .state('food.inventorySetup', {
-            url: "/inventory_setup",
-            templateUrl: "views/inventorySetup.html",
-            data: {pageTitle: 'Inventory Setup'}
-        })
-        .state('food.foodInventory', {
-            url: "/food_inventory",
-            templateUrl: "views/foodInventory.html",
-            data: {pageTitle: 'Food Inventory'}
-        })
-        .state('food.recipeSetup', {
-            url: "/recipe_setup",
-            templateUrl: "views/recipeSetup.html",
-            data: {pageTitle: 'Recipe Setup'}
-        })
-        .state('food.menuSetup', {
-            url: "/menu_setup",
-            templateUrl: "views/menuSetup.html",
-            data: {pageTitle: 'Menu Setup'}
-        })
-        .state('food.deliverySetup', {
-            url: "/delivery_setup",
-            templateUrl: "views/deliverySetup.html",
-            data: {pageTitle: 'Delivery Setup'}
         })
         .state('foodSubCategories', {
             url: "/food_sub_categories",
@@ -315,20 +177,10 @@ function config($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, IdlePro
                 }
             }
         })
-         .state('performanceScore', {
-             url: "/performance_score",
-             templateUrl: "views/performanceScore.html",
-             data: {pageTitle: 'Performance Score'}
-         })
-        .state('user', {
-            abstract: true,
-            url: "/user",
-            templateUrl: "views/common/content-new-user.html"
-        })
-        .state('user.createAccount', {
-            url: "/create_Account",
-            templateUrl: "views/ownerProfileSetup.html",
-            data: {pageTitle: 'Profile'}
+        .state('performanceScore', {
+            url: "/performance_score",
+            templateUrl: "views/performanceScore.html",
+            data: {pageTitle: 'Performance Score'}
         })
 }
 
