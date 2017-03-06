9a416dcbc3e523cf8fdab1abae002a917499a510
Eugen Protsenko <protsenkoe@digicode.net>
2017-03-06 13:08:48 +0200

    fix menu items mapping

--- js/components/reports/menuItemsMapping/menuItemsMapping.component.js
+++ js/components/reports/menuItemsMapping/menuItemsMapping.component.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    function menuItemsMappingController(api, $state, auth, localStorageService, restaurant, $rootScope) {
+    function menuItemsMappingController(api, $state, auth, localStorageService, restaurant, $rootScope, SweetAlert) {
 
         if (!auth.authentication.isLogged) {
             $state.go('home');
@@ -21,7 +21,8 @@
             list_type: 'sold_items',
             paginationCount: 25,
             paginationOffset: 0,
-            total: 0
+            total: 0,
+            isDisabledBtn: true
         };
 
         $rootScope.$on('restaurantSelected', function () {
@@ -83,24 +84,103 @@
 
         that.removeItem = function ($index, item) {
 
-            var m = {
-                matched_items: [{
-                    id: item.id,
-                    name: item.name,
-                    focus_item_id: item.focus_item_id,
-                    menu_item_id: item.menu_item_id,
-                    is_active: item.is_active === 1 ? 0 : 1
-                }]
-            };
+            SweetAlert.swal({
+                    title: "Are you sure?",
+                    text: item.is_active === 1 ? "This item will be moved to Archived" : 'This item will be Restored to Archived',
+                    type: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#ed5565",
+                    confirmButtonText: "Confirm"
+                },
+                function (res) {
+                    if (res) {
+                        var m = {
+                            matched_items: [{
+                                id: item.id,
+                                name: item.name,
+                                focus_item_id: item.focus_item_id,
+                                menu_item_id: item.menu_item_id,
+                                is_active: item.is_active === 1 ? 0 : 1
+                            }]
+                        };
 
-            that.api.setItemsForMap(that.restaurant_id.restaurant_id, m).then(function (res) {
-                if (res.data.data.code === 1000) {
-                    that.getItems();
-                    that.api.report_items_match().then(function (res) {
-                        $rootScope.report_items_match_to_show = res.data.data.items_to_match;
-                    })
-                }
-            }, that.getItems)
+                        that.api.setItemsForMap(that.restaurant_id.restaurant_id, m).then(function (res) {
+                            if (res.data.data.code === 1000) {
+                                that.getItems();
+                                that.api.report_items_match().then(function (res) {
+                                    $rootScope.report_items_match_to_show = res.data.data.items_to_match;
+                                })
+                            }
+                        }, that.getItems)
+                    }
+                });
+
+
+        };
+
+        that.checkAll = function (val) {
+
+            if (val == true) {
+                that.searchModel.isDisabledBtn = false;
+            } else {
+                that.searchModel.isDisabledBtn = true;
+            }
+
+            for (var i = 0; that.items.length > i; i++) {
+                that.items[i].isArchived = val;
+            }
+        };
+
+        that.isDisabledBtn = function (item) {
+            if (item.isArchived == true) {
+                that.searchModel.isDisabledBtn = false;
+            } else {
+                that.searchModel.isDisabledBtn = true;
+            }
+        };
+
+
+        that.deleteChecked = function () {
+
+            SweetAlert.swal({
+                    title: "Are you sure?",
+                    text: that.searchModel.is_active == 1 ? "All selected items will be moved to Archived" : 'All selected items will be Restored to Archived',
+                    type: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#ed5565",
+                    confirmButtonText: "Confirm"
+                },
+                function (res) {
+                    if (res) {
+                        var itemsIsArchived = {};
+                        itemsIsArchived['matched_items'] = [];
+
+                        for (var i = 0; that.items.length > i; i++) {
+                            if (that.items[i].isArchived === true) {
+
+                                itemsIsArchived["matched_items"].push({
+                                    id: that.items[i].id,
+                                    name: that.items[i].name,
+                                    focus_item_id: that.items[i].focus_item_id,
+                                    menu_item_id: that.items[i].menu_item_id,
+                                    is_active: that.items[i].is_active === 1 ? 0 : 1
+                                });
+                            }
+                        }
+
+
+                        that.api.setItemsForMap(that.restaurant_id.restaurant_id, itemsIsArchived).then(function (res) {
+                            if (res.data.data.code === 1000) {
+                                that.getItems();
+                                that.api.report_items_match().then(function (res) {
+                                    $rootScope.report_items_match_to_show = res.data.data.items_to_match;
+                                })
+                            }
+                        }, that.getItems);
+                    }
+                });
+
+
         };
 
         that.$onInit = function () {
@@ -112,7 +192,7 @@
 
     }
 
-    menuItemsMappingController.$inject = ['api', '$state', 'auth', 'localStorageService', 'restaurant', '$rootScope'];
+    menuItemsMappingController.$inject = ['api', '$state', 'auth', 'localStorageService', 'restaurant', '$rootScope', 'SweetAlert'];
 
     angular.module('inspinia').component('menuItemsMappingComponent', {
         templateUrl: 'js/components/reports/menuItemsMapping/menuItemsMapping.html',
