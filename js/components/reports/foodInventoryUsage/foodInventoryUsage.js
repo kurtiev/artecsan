3ceb4d9f7f0e549849e85f3d04e4c51c88ff289a
Fikret Kurtiev <fikretkurtiev@gmail.com>
2017-04-06 18:06:23 +0300

    ARS-67

--- js/components/reports/foodInventoryUsage/foodInventoryUsage.js
+++ js/components/reports/foodInventoryUsage/foodInventoryUsage.js
@@ -7,7 +7,21 @@
             .state('reports.food.inventoryUsage', {
                 url: "/inventory-usage",
                 template: "<inventory-usage></inventory-usage>",
-                data: {pageTitle: 'Inventory Usage'}
+                data: {pageTitle: 'Inventory Usage'},
+                resolve: {
+                    loadPlugin: function ($ocLazyLoad) {
+                        return $ocLazyLoad.load([
+                            {
+                                files: ['js/plugins/moment/moment.min.js']
+                            },
+                            {
+                                name: 'datePicker',
+                                files: ['css/plugins/datapicker/angular-datapicker.css', 'js/plugins/datapicker/angular-datepicker.js']
+                            }
+
+                        ]);
+                    }
+                }
             });
     })
 
