c8913a1a21167c1aad3d1b74b1c7e35c1b2a8f11
Fikret Kurtiev <fikretkurtiev@gmail.com>
2017-04-14 18:36:07 +0300

    ARS-79

--- js/components/alcoholSetup/inventorySetup/addUniqueItems/addUniqueItems.controller.js
+++ js/components/alcoholSetup/inventorySetup/addUniqueItems/addUniqueItems.controller.js
@@ -81,22 +81,21 @@
                 m.sku_items.push({
                     item_name: that.uniqueItem[i].item_name,
                     tare_type_id: that.uniqueItem[i].tare_type_id,  //tare_type_id, refbooks
-                    size: that.uniqueItem[i].content_weight,
+                    size: that.uniqueItem[i].content_weight || (that.uniqueItem[i].full_weight - that.uniqueItem[i].tare_weight),
                     full_weight: that.uniqueItem[i].full_weight,
                     tare_weight: that.uniqueItem[i].tare_weight,
                     manufacturer: that.uniqueItem[i].manufacturer,
                     vendor_sku: that.uniqueItem[i].vendor_sku,
-                    case_qty: 1,
-                    total_unit_size: that.uniqueItem[i].total_unit_size * that.uniqueItem[i].full_weight,
-                    pack: that.uniqueItem[i].total_unit_size,
-                    price: that.uniqueItem[i].case_cost,
-                    // unit_cost: that.uniqueItem[i].unit_cost, todo
+                    case_qty: that.uniqueItem[i].case_qty,
+                    pack: that.uniqueItem[i].pack,
+                    price: that.uniqueItem[i].unit_cost,
                     vendor_cat_id: that.uniqueItem[i].vendor_cat_id,
                     vendor_sub_cat_id: that.uniqueItem[i].vendor_sub_cat_id,
                     is_active: that.uniqueItem[i].is_active,
                     minimum_order_type: 'Each',
                     id: that.uniqueItem[i].id
                 });
+
 
             }
 
@@ -137,7 +136,7 @@
         };
 
         that.calculate = function ($index) {
-            var t = that.uniqueItem[$index].total_unit_size || 0;
+            var t = that.uniqueItem[$index].pack || 0;
             var u = that.uniqueItem[$index].unit_cost || 0;
             that.uniqueItem[$index].case_cost = (t * u);
         };
@@ -157,10 +156,6 @@
                     that.uniqueItem[$index].tare_weight = t;
                     that.uniqueItem[$index].full_weight = f;
                     that.uniqueItem[$index].content_weight = c;
-
-                    if (!t) {
-                        that.uniqueItem[$index].tare_weight = f - c;
-                    }
 
                     break
                 }
